ee0261d9af9a7b2a6a23e378c06a9228a89f6bdc
blomdahldaniel <blomdahl.daniel@gmail.com>
2017-05-17 14:32:09 +0200

    add examples for components

---
+++ examples/Components/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>{{ header }}</h1>
+    <p>Förälderns att göra lista</p>
+    <ul>
+      <li v-for="todo in todos">
+        <h3>{{ todo.title }}</h3>
+        <p>{{ todo.description }}</p>
+      </li>
+    </ul>
+
+    <br><br>
+    <hr>
+    <br><br>
+
+    <div class="child">TEST</div>
+
+    <child name="Lars" v-on:sendTodo="addTodo" gender="male"></child>
+
+    <child name="Elna" v-on:sendTodo="addTodo" gender="female"></child>
+
+    <child name="Olov" v-on:sendTodo="addTodo" gender="male"></child>
+  </div>
+</template>
+
+<script>
+import Child from './components/child.vue'
+export default {
+  name: 'app',
+  components: { Child },
+  data () {
+    return {
+      header: 'Att göra',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo (title, description){
+      this.todos.push({title: title, description: description})
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ examples/Components/Child.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="child" v-bind:class="gender">
+        <h3>{{ name }}</h3>
+        <button @click="sendTodo">I need to pee</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'child',
+    props: ['name', 'gender'],
+    methods: {
+        sendTodo (){
+            this.$emit('sendTodo', 'Bathroom duty', this.name + ' need to pee')
+        }
+    }
+}
+</script>
+
+<style>
+
+.child{
+  padding: 20px;
+  margin: 20px;
+  background: silver;
+}
+.child.male{
+  background: pink;
+}
+.child.female{
+  background: lightblue;
+}
+</style>
